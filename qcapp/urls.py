2b5e40060d42a0fdcad785e134e20f9783466840
anuschka <ana@sustic.com>
2016-03-05 21:10:20 +0100

    Added SimpleItem model to admin interface

--- qcapp/urls.py
+++ qcapp/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 from . import views
 from django.contrib.auth.views import login
+from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # Login / logout.
     url(r'^login/$', login),
     url(r'^logout/$', views.logout_page, name='logout_page'),
+    # Admin site.
+    url(r'^admin/', admin.site.urls),
 ]
