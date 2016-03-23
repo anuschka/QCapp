753bb27ad66631dca5b96e75d35b2406206d53f4
Maksim Sipos <max@kpartite.com>
2016-03-23 16:41:07 +0100

    Some changes.

--- qcapp/urls.py
+++ qcapp/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.auth.views import login
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     # Login / logout.
-    url(r'^login/$', login),
+    url(r'^login/$', views.login_page, name='login_page'),
     url(r'^logout/$', views.logout_page, name='logout_page'),
+    url(r'^register/$', views.register_page, name='register_page'),
     # Admin site.
     url(r'^admin/', admin.site.urls),
 ]
--- qcapp/views.py
+++ qcapp/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
-from django.contrib import auth
-from django.core.context_processors import csrf
-from qcapp.models import UserProfile
+# from qcapp.models import UserProfile
+from django.contrib.auth.models import User
 from qcapp.forms import RegistrationForm
-import datetime, random, sha
-from django.shortcuts import render_to_response, get_object_or_404
+from django.template.response import TemplateResponse
+# import datetime
+
 
 @login_required
 def index(request):
@@ -15,7 +15,8 @@ def index(request):
     If users are authenticated, direct them to the main page. Otherwise, take
     them to the login page.
     """
-    return render_to_response('qcapp/index.html')
+    return TemplateResponse('qcapp/index.html')
+
 
 def logout_page(request):
     """Log users out and re-direct them to the main page."""
@@ -23,32 +24,46 @@ def logout_page(request):
     return HttpResponseRedirect('/')
 
 
+def login_page(request):
+    context = {}
+    form = RegistrationForm()
+    context['form'] = form
+
+    return TemplateResponse(request, 'registration/login.html', context)
+
+
+def register_page(request):
+    form = RegistrationForm(request.POST)
+
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password1']
+
+        user = User.objects.create_user(username, email, password)
+
+        return HttpResponseRedirect('/login/')
+    else:
+        return HttpResponseRedirect('/login/')
+
+
 def register(request):
     if request.user.is_authenticated():
         # They already have an account; don't let them register again
         return render_to_response('register.html', {'has_account': True})
-    manipulator = RegistrationForm()
+    form = RegistrationForm()
     if request.POST:
         new_data = request.POST.copy()
-        errors = manipulator.get_validation_errors(new_data)
+        errors = form.get_validation_errors(new_data)
         if not errors:
-            # Save the user
-            manipulator.do_html2python(new_data)
-            new_user = manipulator.save(new_data)
 
-            # Build the activation key for their account
-            salt = sha.new(str(random.random())).hexdigest()[:5]
-            activation_key = sha.new(salt+new_user.username).hexdigest()
-            key_expires = datetime.datetime.today() + datetime.timedelta(2)
-
-            # Create and save their profile
-            new_profile = UserProfile(user=new_user,
-                                      activation_key=activation_key,
-                                      key_expires=key_expires)
-            new_profile.save()
+            # # Create and save their profile
+            # new_profile = UserProfile(user=new_user,
+            #                           activation_key=activation_key,
+            #                           key_expires=key_expires)
+            # new_profile.save()
 
             return render_to_response('register.html', {'created': True})
     else:
         errors = new_data = {}
-    form = forms.FormWrapper(manipulator, new_data, errors)
     return render_to_response('register.html', {'form': form})
