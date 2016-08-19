2af161af80d9f5b1af647548e057587cf1d5054f
anuschka <ana@sustic.com>
2016-08-19 11:30:05 +0200

    Correcting Flake errors

--- qcapp/views/authentication_views.py
+++ qcapp/views/authentication_views.py
@@ -1,24 +1,13 @@
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout, authenticate, login
 from django.contrib.auth.models import User
 from qcapp.forms import RegistrationForm, LoginForm
 from django.template.response import TemplateResponse
 from django.db import transaction
-from qcapp.models import Cell, UserProfile, CellPanel
-
-
-@login_required
-def index(request):
-    """
-    If users are authenticated, direct them to the main page. Otherwise, take
-    them to the login page.
-    """
-    return TemplateResponse(request, 'index.html', {})
 
 
 def logout_view(request):
-    """Log users out and re-direct them to the main page."""
+    # Log users out and re-direct them to the main page.
     logout(request)
     return HttpResponseRedirect('/')
 
@@ -54,53 +43,9 @@ def login_view(request):
             return TemplateResponse(request, 'login.html', {'form': form})
 
 
-@login_required
-def portal_view(request):
-
-    # get all the CellPanel objects
-    cellpanels = CellPanel.objects.all()
-
-    # get all the Cell objects
-    cells = Cell.objects.filter(cell_panel__manufacturer__contains='DIA-MED')
-
-    # get all the Reagent objects
-    ## get all the idcard objects
-    #reagents = Reagent.objects.all()
-    #idcards = IdCard.objects.all()
-
-    # get all the CellPanel objects for Cell=cell1
-    #cell1 = Cell.objects.filter(number=1)
-    #cellpanels1 = CellPanel.objects.filter(cell=cell1)
-    # get all the CellPanel objects using lists
-    #cellpanel_list = []
-    #for cell in cells:
-    #    cellpanels = CellPanel.objects.filter(cell=cell)
-    #    cellpanel_list += cellpanels
-    # get all the CellPanel objects using dictionaries
-    #cell_cellpanel_map = {}
-    #for cell in cells:
-#        l = []
-#        for cellpanel in cellpanels:
-#            if cellpanel.cell == cell:
-#                l.append(cellpanel)
-#        cell_cellpanel_map[cell] = l
-    context = {
-        'user': request.user,
-        'cell_panels': cellpanels,
-        'cells': cells,
-#         'reagents': reagents,
-#         'idcards': idcards,
-#         'cellpanels1': cellpanels1,
-#         'cellpanel_list': cellpanel_list,
-#         'cell_cellpanel_map': cell_cellpanel_map
-    }
-    return TemplateResponse(request, 'portal.html', context)
-
-
-
 def register_view(request):
     if request.user.is_authenticated():
-    # User is already logged in
+        # User is already logged in
         return HttpResponseRedirect('/portal/')
     if request.method == 'GET':
         form = RegistrationForm()
