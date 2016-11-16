2a04ddcffc9df3326e7abcf2bd4f2147ca06be53
anuschka <ana@sustic.com>
2016-11-16 14:33:55 +0100

    CBV for edit reagent

--- qcapp/views/reagent_views.py
+++ qcapp/views/reagent_views.py
@@ -81,34 +81,54 @@ class ReagentNewView(FormView):
 reagent_new_view = login_required(ReagentNewView.as_view())
 
 
-@login_required
-def reagent_edit_view(request, id):
+# @login_required
+# def reagent_edit_view(request, id):
+#
+#     if request.method == 'GET':
+#         print('edit', id)
+#         obj = get_object_or_404(Reagent.objects.filter(id=id))
+#         form = ReagentForm(instance=obj)
+#         context = {
+#             'active_page': 'reagent',
+#             'id': id,
+#             'form': form
+#         }
+#         return TemplateResponse(request, 'reagents_edit.html', context)
+#     elif request.method == 'POST':
+#         obj = get_object_or_404(Reagent.objects.filter(id=id))
+#
+#         form = ReagentForm(request.POST, instance=obj)
+#         print(form.errors)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/reagent/')
+#         context = {
+#             'active_page': 'reagent',
+#             'id': id,
+#             'form': form
+#         }
+#         return TemplateResponse(request, 'reagents_edit.html', context)
 
-    if request.method == 'GET':
-        print('edit', id)
-        obj = get_object_or_404(Reagent.objects.filter(id=id))
-        form = ReagentForm(instance=obj)
-        context = {
-            'active_page': 'reagent',
-            'id': id,
-            'form': form
-        }
-        return TemplateResponse(request, 'reagents_edit.html', context)
-    elif request.method == 'POST':
-        obj = get_object_or_404(Reagent.objects.filter(id=id))
+class ReagentEditView(FormView):
+    template_name = 'reagents_edit.html'
+    form_class = ReagentForm
 
-        form = ReagentForm(request.POST, instance=obj)
-        print(form.errors)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/reagent/')
-        context = {
-            'active_page': 'reagent',
-            'id': id,
-            'form': form
-        }
-        return TemplateResponse(request, 'reagents_edit.html', context)
+    def get_form(self, form_class):
+        """
+        Check if the user already saved reagent details. If so, then show
+        the form populated with those details, to let user change them.
+        """
+        try:
+            reagent = Reagent.objects.get(id=self.args[0])
+            return form_class(instance=reagent, **self.get_form_kwargs())
+        except Reagent.DoesNotExist:
+            return form_class(**self.get_form_kwargs())
 
+    def form_valid(self, form, **kwargs):
+        form.save()
+        return HttpResponseRedirect('/reagent/')
+
+reagent_edit_view = login_required(ReagentEditView.as_view())
 
 @login_required
 @require_POST
