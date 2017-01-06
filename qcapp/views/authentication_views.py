d8bf7683700a90abfc2191d5be265793d2b30a31
anuschka <ana@sustic.com>
2017-01-06 15:34:40 +0100

    Cleaned register view and template

--- qcapp/views/authentication_views.py
+++ qcapp/views/authentication_views.py
@@ -36,72 +36,27 @@ class LoginView(FormView):
         return HttpResponseRedirect('/portal/')
 login_view = LoginView.as_view()
 
-#def login_view(request):
-#    if request.user.is_authenticated():
-#        # User is already logged in
-#        return HttpResponseRedirect('/portal/')
-#    if request.method == 'GET':
-#        form = LoginForm()
-#        return TemplateResponse(request, 'login.html', {'form': form})
-#    elif request.method == 'POST':
-#        form = LoginForm(request.POST)
-#        if form.is_valid():
-#            username = form.cleaned_data['username']
-#            password = form.cleaned_data['password']
-#            user = authenticate(username=username, password=password)
-#            if user is None:
-#                context = {
-#                    'form': form,
-#                    'error': 'No such user!'
-#                }
-#                return TemplateResponse(request, 'login.html', context)
-#            if not user.is_active:
-#                context = {
-#                    'form': form,
-#                    'error': 'Inactive user!'
-#                }
-#                return TemplateResponse(request, 'login.html', context)
-#            login(request, user)
-#            return HttpResponseRedirect('/portal/')
-#        else:
-#            return TemplateResponse(request, 'login.html', {'form': form})
 
+class RegisterView(FormView):
+    template_name = 'register.html'
+    form_class = RegistrationForm
 
-def register_view(request):
-    if request.user.is_authenticated():
-        # User is already logged in
+    def form_valid(self, form):
+        data = form.cleaned_data
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password1 = form.cleaned_data['password1']
+        password2 = form.cleaned_data['password2']
+
+        if User.objects.filter(username=data['username']).exists():
+            form.add_error(None, 'Username exists!')
+            return self.form_invalid(form)
+        if password1 != password2:
+            form.add_error(None, 'Passwords do not match!')
+            return self.form_invalid(form)
+
+        with transaction.atomic():
+            user = User.objects.create_user(username, email, password1)
+            profile = UserProfile.objects.create(user=user, roles='T')
         return HttpResponseRedirect('/portal/')
-    if request.method == 'GET':
-        form = RegistrationForm()
-        return TemplateResponse(request, 'register.html', {'form': form})
-    elif request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password1 = form.cleaned_data['password1']
-            password2 = form.cleaned_data['password2']
-
-            if User.objects.filter(
-             username=form.cleaned_data['username']).exists():
-                # Username exists
-                context = {
-                    'form': form,
-                    'error': 'Username exists!'
-                }
-                return TemplateResponse(request, 'register.html', context)
-
-            if password1 != password2:
-                # Password is not equal
-                context = {
-                    'form': form,
-                    'error': 'Passwords do not match!'
-                }
-                return TemplateResponse(request, 'register.html', context)
-
-            with transaction.atomic():
-                user = User.objects.create_user(username, email, password1)
-                profile = UserProfile.objects.create(user=user, roles='T')
-            return HttpResponseRedirect('/portal/')
-        else:
-            return TemplateResponse(request, 'register.html', {'form': form})
+register_view = RegisterView.as_view()
