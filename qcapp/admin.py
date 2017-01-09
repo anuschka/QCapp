cb51bc66bc960af90ae8744222a8b52c0e383b97
anuschka <ana@sustic.com>
2017-01-09 10:39:03 +0100

    Register user, password in admin.py

--- qcapp/admin.py
+++ qcapp/admin.py
@@ -5,6 +5,6 @@ from .models import Cell, Reagent, IdCard, CellPanel, UserProfile
 
 admin.site.register(Reagent)
 admin.site.register(IdCard)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, list_display=['user', 'roles'])
 admin.site.register(CellPanel)
 admin.site.register(Cell)
